fc668a3e138e4aa6accd89d9bce395e58d7495b7
Adriano Rodrigo Guerreiro Laranjeira <adriano@argl.eng.br>
2012-08-03 21:11:55 -0300

    Testing...

--- example1.php
+++ example1.php
@@ -6,3 +6,4 @@ $classLoader->register();
 $card_handler = new \Geeklist\Card('arglbr', \Geeklist\OUTPUT_ARRAY);
 print print_r($card_handler->getAllCards(), true) . PHP_EOL;
 print print_r($card_handler->getRandomCard(), true) . PHP_EOL;
+//
