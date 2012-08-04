4ab432a09b2a57f92b85be7b3b0e6596c7a56690
Adriano Laranjeira <adriano@argl.eng.br>
2012-08-04 17:41:08 -0300

    Putting an autoloader class again, refactoring the example file.

--- example1.php
+++ example1.php
@@ -1,10 +1,33 @@
 <?php
-require 'Geeklist/Loader/Autoloader.php';
-$classLoader = new \Geeklist\Loader\Autoloader('Geeklist', dirname(__FILE__) . '');
+/*
+ * Instead the way above, you can use the two lines below
+ * if you're using Composer (http://getcomposer.org):
+ * $loader = require 'vendor/autoload.php';
+ * $loader->add('Geeklist', '/path/to/geeklist-php');
+*/
+require __DIR__ . '/Geeklist/SPLClassLoader.php';
+$classLoader = new \Geeklist\SPLClassLoader('Geeklist', __DIR__);
 $classLoader->register();
 
+/*
+ * \Geeklist\Card(username, OUTPUT_FORMAT);
+ * Constants for OUTPUT_FORMAT:
+ *    \Geeklist\OUTPUT_ARRAY - For output as array;
+ *    \Geeklist\OUTPUT_JSON  - For output as JSON.
+*/
 $card_handler = new \Geeklist\Card('arglbr', \Geeklist\OUTPUT_ARRAY);
-print print_r($card_handler->getAllCards(), true) . PHP_EOL;
-print print_r($card_handler->getRandomCard(), true) . PHP_EOL;
-//
-//
+
+/*
+ * All this horn for two methods: :-)
+ *    \Geeklist\Card::getAllCards():
+ *    Fetch all cards for a given user;
+ *
+ *    \Geeklist\Card::getRandomCard():
+ *    Fetch a random card for a given user,
+*/
+try {
+    print print_r($card_handler->getAllCards(), true) . PHP_EOL;
+    print print_r($card_handler->getRandomCard(), true) . PHP_EOL;
+} catch (Exception $e) {
+    print 'Exception raised with the message: ' . $e->getMessage() . str_repeat(PHP_EOL, 2);
+}
